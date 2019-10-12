35e4a5f9815dfdda782fd65e55b7ccd9247f3541
rutuja027 <40690593+rutuja027@users.noreply.github.com>
2019-10-12 23:27:26 +0530

    Add files via upload

---
+++ Chapter04/Exercise03/students_sorted_by_age.clj
@@ -0,0 +1,8 @@
+(def students [{:name "Eliza" :year 1994}
+               {:name "Salma" :year 1995}
+               {:name "Jodie" :year 1997}
+               {:name "Kaitlyn" :year 2000}
+               {:name "Alice" :year 2001}
+               {:name "Pippa" :year 2002}
+               {:name "Fleur" :year 2002}])
+
