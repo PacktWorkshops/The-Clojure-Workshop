291a4955be896237b292d2061bb4a8b3b79a8834
Joseph Fahey <fahey.joseph@gmail.com>
2020-01-29 23:25:26 +0100

    Ch04: missing function in Exercise 4.05

--- Chapter04/Exercise4.05/repl.clj
+++ Chapter04/Exercise4.05/repl.clj
@@ -1,4 +1,9 @@
 ;;; In REPL
 (def our-randoms (repeatedly (partial rand-int 100)))
+
+(defn some-random-integers [size]
+    (take size (repeatedly (fn [] (rand-int 100)))))
+
+
 (take 20 our-randoms)
 (some-random-integers 12)
