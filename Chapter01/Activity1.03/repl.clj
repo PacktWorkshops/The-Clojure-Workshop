c44c165b51b9fc1a0f6a2065ab4212b07b22e3d4
Yan <yan@metatem.net>
2022-07-23 01:52:37 +0200

    Correct Activity 1.03
    According to the specifications:
    
    The function will print a transformation of the string passed as a
    _second_ argument based on the calmness level.
    
    If the calmness level is strictly inferior to _5_, we consider the
    user to be angry.
    
    Also corrected the test, and added assertions for the limit values.

--- Chapter01/Activity1.03/repl.clj
+++ Chapter01/Activity1.03/repl.clj
@@ -1,29 +1,29 @@
 ;; 6
 (defn meditate
   "Return a transformed version of the string `s` based on the `calmness-level`"
-  [s calmness-level]
+  [calmness-level s]
   (println "Clojure Meditate v2.0")
-  (if (< calmness-level 4)
+  (if (< calmness-level 5)
     (str (clojure.string/upper-case s) ", I TELL YA!")
-    (if (<= 4 calmness-level 9)
+    (if (<= 5 calmness-level 9)
         (clojure.string/capitalize s)
         (if (= 10 calmness-level)
           (clojure.string/reverse s)))))
 
 ;; 7
-(meditate "what we do now echoes in eternity" 1)
-(meditate "what we do now echoes in eternity" 6)
-(meditate "what we do now echoes in eternity" 10)
-(meditate "what we do now echoes in eternity" 50)
+(meditate 1 "what we do now echoes in eternity")
+(meditate 6 "what we do now echoes in eternity")
+(meditate 10 "what we do now echoes in eternity")
+(meditate 50 "what we do now echoes in eternity")
 
 ;; 9
 ; (doc cond)
 
 (defn meditate
   "Return a transformed version of the string `s` based on the `calmness-level`"
-  [s calmness-level]
+  [calmness-level s]
   (println "Clojure Meditate v2.0")
   (cond
-    (< calmness-level 4)  (str (clojure.string/upper-case s) ", I TELL YA!")
-    (<= 4 calmness-level 9) (clojure.string/capitalize s)
-    (= 10 calmness-level) (clojure.string/reverse s))) 
+    (< calmness-level 5) (str (clojure.string/upper-case s) ", I TELL YA!")
+    (<= 5 calmness-level 9) (clojure.string/capitalize s)
+    (= 10 calmness-level) (clojure.string/reverse s)))
--- Chapter01/tests/Activity1.03/repl.clj
+++ Chapter01/tests/Activity1.03/repl.clj
@@ -4,9 +4,12 @@
 
 (deftest meditation-test
   (are [x y] (= x y)
-    "WHAT WE DO NOW ECHOES IN ETERNITY, I TELL YA!" (meditate "what we do now echoes in eternity" 1)
-    "What we do now echoes in eternity" (meditate "what we do now echoes in eternity" 6)
-    "ytinrete ni seohce won od ew tahw" (meditate "what we do now echoes in eternity" 10)
-    nil (meditate "what we do now echoes in eternity" 50)))
+    "WHAT WE DO NOW ECHOES IN ETERNITY, I TELL YA!" (meditate 1 "what we do now echoes in eternity")
+    "WHAT WE DO NOW ECHOES IN ETERNITY, I TELL YA!" (meditate 4 "what we do now echoes in eternity")
+    "What we do now echoes in eternity" (meditate 5 "what we do now echoes in eternity")
+    "What we do now echoes in eternity" (meditate 6 "what we do now echoes in eternity")
+    "What we do now echoes in eternity" (meditate 9 "what we do now echoes in eternity")
+    "ytinrete ni seohce won od ew tahw" (meditate 10 "what we do now echoes in eternity")
+    nil (meditate 50 "what we do now echoes in eternity")))
 
  (run-tests)
